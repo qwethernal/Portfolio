06b2deb795259c4720b10d10c3ed75f4017871a0
qwethernal <132042328+qwethernal@users.noreply.github.com>
2024-10-22 14:52:10 +0300

    Add files via upload

---
+++ build/precache-manifest.6888debef6345a68dc5e6f0853f05e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "371bdf2e52c371631b6d",
+    "url": "qwethernal.github.io/Portfolio/todo/static/css/main.ff5cba3f.chunk.css"
+  },
+  {
+    "revision": "371bdf2e52c371631b6d",
+    "url": "qwethernal.github.io/Portfolio/todo/static/js/main.371bdf2e.chunk.js"
+  },
+  {
+    "revision": "9621ca5af5e5dba96ad0",
+    "url": "qwethernal.github.io/Portfolio/todo/static/css/1.450ad9d5.chunk.css"
+  },
+  {
+    "revision": "9621ca5af5e5dba96ad0",
+    "url": "qwethernal.github.io/Portfolio/todo/static/js/1.9621ca5a.chunk.js"
+  },
+  {
+    "revision": "7ed981a802d94ad2d36e",
+    "url": "qwethernal.github.io/Portfolio/todo/static/js/runtime~main.7ed981a8.js"
+  },
+  {
+    "revision": "f620ef898e7748abe6ec1d0cf9164d52",
+    "url": "qwethernal.github.io/Portfolio/todo/index.html"
+  }
+];
