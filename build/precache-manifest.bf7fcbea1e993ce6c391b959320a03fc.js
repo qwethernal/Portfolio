afb90786a358f8b3a858a52f91b3d2decec9bbf2
qwethernal <132042328+qwethernal@users.noreply.github.com>
2024-10-22 15:01:56 +0300

    Add files via upload

---
+++ build/precache-manifest.bf7fcbea1e993ce6c391b959320a03fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbd2ca3fd3620a273862",
+    "url": "qwethernal/Portfolio/tree/main/build/static/css/main.afd6e384.chunk.css"
+  },
+  {
+    "revision": "cbd2ca3fd3620a273862",
+    "url": "qwethernal/Portfolio/tree/main/build/static/js/main.cbd2ca3f.chunk.js"
+  },
+  {
+    "revision": "2cc67ba4fdb615955b2f",
+    "url": "qwethernal/Portfolio/tree/main/build/static/css/1.dd223edc.chunk.css"
+  },
+  {
+    "revision": "2cc67ba4fdb615955b2f",
+    "url": "qwethernal/Portfolio/tree/main/build/static/js/1.2cc67ba4.chunk.js"
+  },
+  {
+    "revision": "0b9c8108ddce270e5f67",
+    "url": "qwethernal/Portfolio/tree/main/build/static/js/runtime~main.0b9c8108.js"
+  },
+  {
+    "revision": "5c205215a8eccad6ad6ccad4850c6df7",
+    "url": "qwethernal/Portfolio/tree/main/build/index.html"
+  }
+];
